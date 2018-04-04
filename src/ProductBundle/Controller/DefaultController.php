e549f16a861ceb62e2ba1100e4f7e7680a4c92d9
David Martínez Gilo <davidmgilo@gmail.com>
2018-04-04 20:43:56 +0200

    create product

--- src/ProductBundle/Controller/DefaultController.php
+++ src/ProductBundle/Controller/DefaultController.php
@@ -10,4 +10,21 @@ class DefaultController extends Controller
     {
         return $this->render('ProductBundle:Default:index.html.twig');
     }
+    
+    public function createAction($nom,$descripcio,$preu)
+    {
+        $product = new \ProductBundle\Entity\Producte;
+        $product->setNom($nom);
+        $product->setDescripcio($descripcio);
+        $product->setPreu($preu);
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($product);
+        $flush = $em->flush();
+        
+        if($flush == null){
+            echo "Creat correctament";
+        }
+        die();
+    }
 }
